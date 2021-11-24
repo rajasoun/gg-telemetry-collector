c084fd0cbf940850a4904100a299444162c0d2d0
Raja Soundaramourty <rajasoun@icloud.com>
2021-11-24 15:06:43 +0530

    initial checkin

---
+++ get_secrets_by_repos.bash
@@ -0,0 +1,28 @@
+#!//usr/bin/env bash
+
+# Purpose: Get Secrets for Repos from GG
+# pre-requsites:
+#   brew install jq dasel
+# ------------------------------------------
+
+source .env 
+curl -H "Authorization: Token ${GITGUARDIAN_API_KEY}" "${GITGUARDIAN_API_URL}/v1/sources?type=github&&page=1&per_page=200" | jq  > "data/data.json"
+dasel -r json -w csv < "data/data.json" > "data/data.csv"
+
+INPUT="data/data.csv"
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+
+echo "id,full_name,secrets_count" > "result/data.csv"
+while read full_name id	type url visibility
+do
+	echo "Getting Secrets For Repo $full_name with ID : $id"
+    secrets_count=$(curl -H "Authorization: Token ${GITGUARDIAN_API_KEY}" "${GITGUARDIAN_API_URL}/v1/occurrences/secrets?source_id=$id"  |  jq -c '.[] | select(  .presence == "present")  ' | wc -l)
+    echo "Secrets Count: $secrets_count"
+    echo "$id,$full_name,$secrets_count" >> "result/data.csv"
+done < $INPUT
+IFS=$OLDIFS
+
+
+
