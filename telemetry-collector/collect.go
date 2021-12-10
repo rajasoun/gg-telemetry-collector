4d8ebe7b96c43174aa5f9a4b49133a2f9389b7e1
Raja Soundaramourty <rajasoun@icloud.com>
2021-12-10 06:12:58 +0000

    feat(tooling): container support

--- telemetry-collector/collect.go
+++ telemetry-collector/collect.go
@@ -34,14 +34,18 @@ func getContentFromFile(fileName string) string {
 	return cookieContent
 }
 
-func main() {
+func loadDotEnv() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+func main() {
+	loadDotEnv()
+
 	client := &http.Client{}
-	testRepoName := os.Getenv("TEST_REPO_NAME")
-	url := formWebEndPointForRepo(testRepoName)
+	url := formWebEndPointForRepo(os.Getenv("TEST_REPO_NAME"))
 	method := "GET"
 
 	req, err := http.NewRequest(method, url, nil)
